9f8d0ab2d2339ebbcc4667a1d878cc78c5b85e23
consmariano <constanzamrsm@gmail.com>
2020-04-18 01:14:25 -0300

    Adding word counter

---
+++ treinamento-ntec/ppwer/js/feed.js
@@ -0,0 +1,16 @@
+// Busca texto
+var textboxPiupeet = (document).getElementById("#validation");
+
+var contador = (document).getElementById("#wordCount");
+
+textboxPiupeet.addEventListener("keyup", counter);
+
+function counter(characters) {
+    var characters = textboxPiupeet.value.split(''); //lista contendo todos os characters digitados pelo usuário
+    contador.innerText = characters.length;
+    if (characters.lenght > 140){
+        textboxPiupeet.addClass("textbox-invalid-piupeet");
+    }
+}
+
+console.log(textboxPiupeet);
